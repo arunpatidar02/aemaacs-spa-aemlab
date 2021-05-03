6ee748d2ce2fe5200b179a6cd42baf1cded4e4b9
Arun Patidar <arunpatidar26@gmail.com>
2021-05-03 21:21:29 +0200

    card js

--- core/src/main/java/com/community/aemlab/spa/core/models/LinkModel.java
+++ core/src/main/java/com/community/aemlab/spa/core/models/LinkModel.java
@@ -1,9 +1,13 @@
 package com.community.aemlab.spa.core.models;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import com.community.aemlab.spa.core.utils.ModelHelper;
 
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class LinkModel {
@@ -14,6 +18,13 @@ public class LinkModel {
 	@ValueMapValue
 	private String linkURL;
 
+	@PostConstruct
+	protected void init() {
+		if (linkURL != null) {
+			linkURL = ModelHelper.getLink(linkURL);
+		}
+	}
+
 	public String getLinkURL() {
 		return linkURL;
 	}
@@ -21,6 +32,5 @@ public class LinkModel {
 	public String getLinkText() {
 		return linkText;
 	}
-
 
 }
--- ui.frontend/src/components/Cards/Cards.js
+++ ui.frontend/src/components/Cards/Cards.js
@@ -1,47 +1,57 @@
- import { MapTo } from '@adobe/aem-react-editable-components';
- import DOMPurify from 'dompurify';
- import React, { Component } from 'react';
- import extractModelId from '../../utils/extract-model-id';
- 
- require('./Cards.css');
- 
- /**
-  * Default Edit configuration for the Cards component
-  *
-  * @type EditConfig
-  */
- const CardsEditConfig = {
-   emptyLabel: 'Cards',
- 
-   isEmpty: function(props) {
-    return !props || !props.cardTitle;
-   }
- };
- 
- /**
-  * Cards React component
-  */
+import { MapTo } from '@adobe/aem-react-editable-components';
+import React, { Component } from 'react';
+import DOMPurify from 'dompurify';
+import extractModelId from '../../utils/extract-model-id';
 
- class Cards extends Component {
-    get title() {
-        return <div>{this.props.cardTitle}</div>;
-      }
-      get description() {
-        return <div>{this.props.cardDescription}</div>;
-      }
-    
-      render() {
+require('./Cards.css');
+
+/**
+ * Default Edit configuration for the Cards component
+ *
+ * @type EditConfig
+ */
+const CardsEditConfig = {
+    emptyLabel: 'Cards',
+
+    isEmpty: function (props) {
+        return !props || !props.cardTitle || !props.cardDescription || !props.cardLink;
+    }
+};
+
+/**
+ * Cards React component
+ */
+
+class Cards extends Component {
+    get description() {
         return (
-    <>
-    <div>{this.title}</div>
-    <div>{this.description}</div>
-    </>
+            <div
+                id={extractModelId(this.props.cqPath)}
+                data-rte-editelement
+                dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(this.props.cardDescription)
+                }}
+            />
         );
-      }
- }
- 
- export default MapTo('aemlab-spa/components/custom/cards')(
+    }
+
+    get cta() {
+        return this.props.cardLink ? <a href={this.props.cardLink.linkURL}>{this.props.cardLink.linkText}</a> : "";
+    }
+
+    render() {
+        return (
+            <div className="cards_container">
+                <div className="cards_title">{this.props.cardTitle}</div>
+                <div className="cards_description">{this.description}</div>
+
+                <div className="cards_cta">{this.cta}</div>
+            </div>
+        );
+    }
+}
+
+export default MapTo('aemlab-spa/components/custom/cards')(
     Cards,
     CardsEditConfig
- );
- 
+);
